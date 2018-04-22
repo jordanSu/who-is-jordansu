9f87bd85157e9248548584d7a740da4d129d418c
jordanSu <newjordansu1126@gmail.com>
2018-04-22 16:57:33 +0800

    try echo bot again

--- index.js
+++ index.js
@@ -2,27 +2,38 @@ const TOKEN = process.env.access_token;
 const SECRET = process.env.channel_secret;
 const PORT = process.env.PORT | 8080;
 
-var express = require('express');
-var logfmt = require('logfmt');
-var bodyParser = require('body-parser');
+const express = require('express');
+const logfmt = require('logfmt');
+const bodyParser = require('body-parser');
 const app = express();
+
+const line = require('@line/bot-sdk');
+const client = new line.Client({
+  channelAccessToken: TOKEN
+});
 
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
 
+// Make sure Server alive
 app.get('/', function(req, res) {
-	//res.send('Server alive!');
 	res.status(200).end("Server alive!");
 	console.log(req.body);
 });
 
 app.post('/', function(req, res) {
-	console.log(req.body);
-	res.status(200).end("你好");
+	console.log(req.body.text);
+	var message = {type: 'text', text: req.body.text};
+	client.replyMessage(req.body.replyToken, message)
+			.then(() => {
+				console.log("Message: " + message.text);
+			})
+			.catch((err) => {
+				console.err("Send reply error!");
+			})
 });
 
 var port = Number(process.env.PORT || 8080);
 app.listen(port, function() {
 	console.log("Listening on ", port);
 });
-
