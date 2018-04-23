71efcd3028eb87839b3a71fb79803e5d186b43d8
jordanSu <newjordansu1126@gmail.com>
2018-04-23 09:32:30 +0800

    auto update

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const client = new line.Client({
   channelAccessToken: TOKEN
 });
 
+const line
+
 
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
@@ -26,7 +28,7 @@ app.post('/', function(req, res) {
 	var webhook_obj = req.body.events[0];
 	console.log(webhook_obj);
 	var message = {type: 'text', text: webhook_obj.message.text};
-	client.replyMessage(webhook_obj.replyToken, message)
+	client.replyMessage(webhook_obj.replyToken, [message, getCarousell()])
 			.then(() => {
 				console.log("Message: " + message.text);
 			})
@@ -39,3 +41,42 @@ var port = Number(process.env.PORT || 8080);
 app.listen(port, function() {
 	console.log("Listening on ", port);
 });
+
+function getCarousell() {
+	return {
+		type: "template",
+		template: {
+			type: "image_carousel",
+			columns: [
+          		{
+            		"imageUrl": "https://example.com/bot/images/item1.jpg",
+            		"action": {
+              			"type": "postback",
+              			"label": "Buy",
+              			"data": "action=buy&itemid=111"
+            		}
+          		},
+          		{
+            		"imageUrl": "https://example.com/bot/images/item2.jpg",
+            		"action": {
+              			"type": "message",
+              			"label": "Yes",
+              			"text": "yes"
+            		}
+          		},
+          		{
+            		"imageUrl": "https://example.com/bot/images/item3.jpg",
+            		"action": {
+              			"type": "uri",
+              			"label": "View detail",
+              			"uri": "http://example.com/page/222"
+            		}
+          		}
+      		]
+		}
+	}
+}
+
+function parseMessage(message) {
+
+}
