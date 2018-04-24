6de4887b76d30500d6ddf6c0087e435b1ad344ba
jordanSu <newjordansu1126@gmail.com>
2018-04-24 19:09:39 +0800

    commit for v0.9

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const logfmt = require('logfmt');
 const bodyParser = require('body-parser');
 const line = require('@line/bot-sdk');
 const fs = require('fs');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const func =  require('./line.js');
 
 const personal_data = JSON.parse(fs.readFileSync('introduction.json', 'utf8'));
 const keywords_list = JSON.parse(fs.readFileSync('keyword.json', 'utf8'));
 
-
 const app = express();
+/*
 const client = new line.Client({
   channelAccessToken: TOKEN
 });
-
+*/
 
 app.use(logfmt.requestLogger());
 app.use(bodyParser.json());
@@ -37,35 +38,12 @@ app.post('/', function(req, res) {
 		res.status(401).end();
 	}
 	else {
-		var replyMessage = [];
 		var webhook_obj = req.body.events[0];
 		console.log(webhook_obj);
 
-		var keyword = parseMessage(webhook_obj.message.text);
-		switch(keyword) {
-			case "cv":
-				replyMessage.push({type: 'text', text: "Hello, 這是我的履歷連結喔～"});
-				replyMessage.push({type: 'text', text: personal_data.cv});
-				break;
-			case "education":
-				replyMessage.push({type: 'text', text: "Hello, 這是我的學歷唷～"});
-				replyMessage.push({type: 'text', text: personal_data.education});
-				break;
-			case "award":
-				replyMessage.push({type: 'text', text: "Hello\n這是我的比賽與得獎經歷喔～"});
-				replyMessage.push({type: 'text', text: personal_data.award});
-				break;
-			case "internship":
-				replyMessage.push({type: 'text', text: "Hello, 這是我的實習經驗喔～"});
-				replyMessage.push({type: 'text', text: personal_data.internship});
-				break;
-			case "who":
-				replyMessage.push({type: 'text', text: personal_data.who});
-			default:
-				replyMessage.push({type: 'text', text: "您可以透過以下了解更多我的資訊喔～"});
-				replyMessage.push(getCarousell());
-				break;
-		}
+		var keyword = func.parseMessage(webhook_obj.message.text);
+		var replyMessge = func.getReplyMessage(keyword);
+
 		//var message = {type: 'text', text: webhook_obj.message.text};
 		client.replyMessage(webhook_obj.replyToken, replyMessage)
 				.then(() => {
@@ -73,7 +51,7 @@ app.post('/', function(req, res) {
 				})
 				.catch((err) => {
 					console.log("Send reply error!");
-				})
+				});
 	}
 });
 
@@ -81,72 +59,3 @@ var port = Number(process.env.PORT || 8080);
 app.listen(port, function() {
 	console.log("Listening on", port);
 });
-
-function getCarousell() {
-	// 內容：履歷，學歷，實習經驗，比賽或其他得獎經歷
-	return {
-		type: "template",
-		altText: "你可以做些什麼？",
-		template: {
-			type: "carousel",
-			imageSize: "contain",
-			columns: [
-          		{
-            		"thumbnailImageUrl": "https://cdn1.iconfinder.com/data/icons/business-seo-vol-1/512/CV_Contract_Agreement_Resume_Paper_Document-512.png",
-					"imageBackgroundColor": "#009999",
-					"text": "您可以查看我的履歷～",
-					"actions": [
-						{
-              				"type": "uri",
-              				"label": "看看履歷",
-              				"uri": personal_data.cv
-						},
-					]
-          		},
-          		{
-            		"thumbnailImageUrl": "https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/128/graduationcap.png",
-					"imageBackgroundColor": "#009999",
-					"text": "您可以查看我的學歷～",
-					"actions": [
-						{
-              				"type": "message",
-              				"label": "看看學歷",
-              				"text": "看看學歷"
-            			}
-					]
-          		},
-          		{
-            		"thumbnailImageUrl": "https://cdn.iconscout.com/public/images/icon/premium/png-256/sharing-experience-thought-communication-relation-348938d2d9d609f5-256x256.png",
-					"imageBackgroundColor": "#009999",
-					"text": "您可以了解我的實習經驗～",
-					"actions": [
-						{
-              				"type": "message",
-              				"label": "看看實習經驗",
-              				"text": "看看實習經驗"
-            			}
-					]
-          		},
-				{
-            		"thumbnailImageUrl": "https://cdn4.iconfinder.com/data/icons/trophy-and-awards-1/64/Icon_Medal_Trophy_Awards_Blue-256.png",
-					"imageBackgroundColor": "#009999",
-					"text": "您可以了解我的比賽與得獎經歷～",
-					"actions": [
-						{
-              				"type": "message",
-              				"label": "比賽與得獎經歷",
-              				"text": "比賽與得獎經歷"
-            			}
-					]
-          		}
-      		]
-		}
-	};
-}
-
-function parseMessage(message) {
-	for(var keyword in keywords_list) {
-		if(keywords_list[keyword].some((word) => message.includes(word)))
-			return keyword;
-	}
-}
---
+++ line.js
@@ -0,0 +1,104 @@
+module.exports = {
+    getCarousell: function() {
+        return {
+    		type: "template",
+    		altText: "你可以做些什麼？",
+    		template: {
+    			type: "carousel",
+    			imageSize: "contain",
+    			columns: [
+              		{
+                		"thumbnailImageUrl": "https://cdn1.iconfinder.com/data/icons/business-seo-vol-1/512/CV_Contract_Agreement_Resume_Paper_Document-512.png",
+    					"imageBackgroundColor": "#0099AA",
+    					"text": "您可以查看我的履歷～",
+    					"actions": [
+    						{
+                  				"type": "uri",
+                  				"label": "看看履歷",
+                  				"uri": personal_data.cv
+    						},
+    					]
+              		},
+              		{
+                		"thumbnailImageUrl": "https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/128/graduationcap.png",
+    					"imageBackgroundColor": "#0099AA",
+    					"text": "您可以查看我的學歷～",
+    					"actions": [
+    						{
+                  				"type": "message",
+                  				"label": "看看學歷",
+                  				"text": "看看學歷"
+                			}
+    					]
+              		},
+              		{
+                		"thumbnailImageUrl": "https://cdn.iconscout.com/public/images/icon/premium/png-256/sharing-experience-thought-communication-relation-348938d2d9d609f5-256x256.png",
+    					"imageBackgroundColor": "#0099AA",
+    					"text": "您可以了解我的實習經驗～",
+    					"actions": [
+    						{
+                  				"type": "message",
+                  				"label": "看看實習經驗",
+                  				"text": "看看實習經驗"
+                			}
+    					]
+              		},
+    				{
+                		"thumbnailImageUrl": "https://cdn4.iconfinder.com/data/icons/trophy-and-awards-1/64/Icon_Medal_Trophy_Awards_Blue-256.png",
+    					"imageBackgroundColor": "#0099AA",
+    					"text": "您可以了解我的比賽與得獎經歷～",
+    					"actions": [
+    						{
+                  				"type": "message",
+                  				"label": "比賽與得獎經歷",
+                  				"text": "比賽與得獎經歷"
+                			}
+    					]
+              		}
+          		]
+    		}
+    	};
+    },
+    parseMessage: function(message) {
+        for(var keyword in keywords_list) {
+    		if(keywords_list[keyword].some((word) => message.includes(word)))
+    			return keyword;
+    	}
+    },
+    getReplyMessage: function(keyword) {
+        var replyMessage = [];
+    	switch(keyword) {
+    		case "cv":
+    			replyMessage.push({type: 'text', text: "Hello, 這是我的履歷連結喔～"});
+    			replyMessage.push({type: 'text', text: personal_data.cv});
+    			break;
+    		case "education":
+    			replyMessage.push({type: 'text', text: "Hello, 這是我的學歷唷～"});
+    			personal_data.education.forEach((message) => {
+    				replyMessage.push({type: 'text', text: message});
+    			});
+    			break;
+    		case "award":
+    			replyMessage.push({type: 'text', text: "Hello\n這是我的比賽與得獎經歷喔～"});
+    			personal_data.award.forEach((message) => {
+    				replyMessage.push({type: 'text', text: message});
+    			});
+    			break;
+    		case "internship":
+    			replyMessage.push({type: 'text', text: "Hello, 這是我的實習經驗喔～"});
+    			personal_data.internship.forEach((message) => {
+    				replyMessage.push({type: 'text', text: message});
+    			});
+    			break;
+    		case "who":
+    			replyMessage.push({type: 'text', text: personal_data.who});
+    		case "interest":
+    			replyMessage.push({type: 'text', text: personal_data.interest});
+    		default:
+    			replyMessage.push({type: 'text', text: "您可以透過以下了解更多我的資訊喔～"});
+    			replyMessage.push(getCarousell());
+    			break;
+    	}
+    	return replyMessge;
+    }
+};
