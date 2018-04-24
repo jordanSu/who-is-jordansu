7cae976aed5428c3d6c046c07d992a0ad28b2332
jordanSu <newjordansu1126@gmail.com>
2018-04-24 14:08:09 +0800

    auto update

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const logfmt = require('logfmt');
 const bodyParser = require('body-parser');
 const line = require('@line/bot-sdk');
 const fs = require('fs');
+const crypto = require('crypto')
 
 const personal_data = JSON.parse(fs.readFileSync('introduction.json', 'utf8'));
+const keyword = JSON.parse(fs.readFileSync('keyword.json', 'utf8'));
 
 const app = express();
 const client = new line.Client({
@@ -25,16 +27,21 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-	var webhook_obj = req.body.events[0];
-	console.log(webhook_obj);
-	var message = {type: 'text', text: webhook_obj.message.text};
-	client.replyMessage(webhook_obj.replyToken, [message, getCarousell()])
-			.then(() => {
-				console.log("Message: " + message.text);
-			})
-			.catch((err) => {
-				console.log("Send reply error!");
-			})
+	var signature = crypto.createHmac("SHA256", SECRET).update(req.body.toString()).digest('base64');
+	if (signature != req.header('X-Line-Signature'))
+		res.status(401).end();
+	else {
+		var webhook_obj = req.body.events[0];
+		console.log(webhook_obj);
+		var message = {type: 'text', text: webhook_obj.message.text};
+		client.replyMessage(webhook_obj.replyToken, [message, getCarousell()])
+				.then(() => {
+					console.log("Message: " + message.text);
+				})
+				.catch((err) => {
+					console.log("Send reply error!");
+				})
+	}
 });
 
 var port = Number(process.env.PORT || 8080);
@@ -78,7 +85,7 @@ function getCarousell() {
           		{
             		"thumbnailImageUrl": "https://cdn.iconscout.com/public/images/icon/premium/png-256/sharing-experience-thought-communication-relation-348938d2d9d609f5-256x256.png",
 					"imageBackgroundColor": "#009999",
-					"text": "您可以'了解我的實習經驗'～",
+					"text": "您可以了解我的實習經驗～",
 					"actions": [
 						{
               				"type": "message",
@@ -90,7 +97,7 @@ function getCarousell() {
 				{
             		"thumbnailImageUrl": "https://cdn4.iconfinder.com/data/icons/trophy-and-awards-1/64/Icon_Medal_Trophy_Awards_Blue-256.png",
 					"imageBackgroundColor": "#009999",
-					"text": "您可以'了解我的比賽與得獎經歷'～",
+					"text": "您可以了解我的比賽與得獎經歷～",
 					"actions": [
 						{
               				"type": "message",
@@ -105,5 +112,5 @@ function getCarousell() {
 }
 
 function parseMessage(message) {
-
+	
 }
